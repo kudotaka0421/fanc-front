a9cc4b29f21d6a2c162ce7b4f62cbd7d6a96d0b4
工藤隆弘 <>
2023-04-23 14:00:53 +0900

    feat: 残りの部分実装　

--- src/App.vue
+++ src/App.vue
@@ -26,11 +26,16 @@ import { ChevronDownIcon, MagnifyingGlassIcon } from "@heroicons/vue/20/solid";
 
 const navigation = [
     { name: "スクールを見つける", href: "#", icon: HomeIcon, current: false },
-    { name: "カウンセリングを予約する", href: "#", icon: UsersIcon, current: false },
+    {
+        name: "カウンセリングを予約する",
+        href: "#",
+        icon: UsersIcon,
+        current: false,
+    },
     { name: "カウンセリング一覧", href: "#", icon: FolderIcon, current: false },
     { name: "ユーザー一覧", href: "#", icon: CalendarIcon, current: false },
     { name: "スタッフ一覧", href: "#", icon: CalendarIcon, current: false },
-    { name: "決済一覧", href: "#", icon: CalendarIcon, current: false }
+    { name: "決済一覧", href: "#", icon: CalendarIcon, current: false },
 ];
 
 const userNavigation = [
@@ -216,6 +221,18 @@ const sidebarOpen = ref(false);
                                     </a>
                                 </li>
                             </ul>
+                        </li>
+                        <li class="mt-auto">
+                            <a
+                                href="#"
+                                class="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-indigo-200 hover:bg-indigo-700 hover:text-white"
+                            >
+                                <Cog6ToothIcon
+                                    class="h-6 w-6 shrink-0 text-indigo-200 group-hover:text-white"
+                                    aria-hidden="true"
+                                />
+                                Settings
+                            </a>
                         </li>
                     </ul>
                 </nav>

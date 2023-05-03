e2abac95aafeb58fd2a91a1e7618db5054443f45
工藤隆弘 <>
2023-05-03 18:14:58 +0900

    feat: スタッフ一覧API繋ぎこみ

--- src/pages/Staffs/Staffs.vue
+++ src/pages/Staffs/Staffs.vue
@@ -6,50 +6,7 @@
 <script setup lang="ts">
 import Breadcrumbs from "../../components/Breadcrumbs.vue";
 import StaffList from "../../components/Staff/StaffList.vue";
+import { useStaffs } from "./useStaffs";
 
-const pages = [{ name: "スタッフ一覧", href: "staffs", current: true }];
-
-// TODO Mockデータのため不要になったら削除する
-const staffs = [
-    {
-        id: 1,
-        firstName: "1-サンプル",
-        lastName: "太郎",
-        firstNameKana: "サンプル",
-        lastNameKana: "タロウ",
-        email: "sample@test.com",
-    },
-    {
-        id: 2,
-        firstName: "2-サンプル",
-        lastName: "太郎",
-        firstNameKana: "サンプル",
-        lastNameKana: "タロウ",
-        email: "sample@test.com",
-    },
-    {
-        id: 3,
-        firstName: "3-サンプル",
-        lastName: "太郎",
-        firstNameKana: "サンプル",
-        lastNameKana: "タロウ",
-        email: "sample@test.com",
-    },
-    {
-        id: 4,
-        firstName: "4-サンプル",
-        lastName: "太郎",
-        firstNameKana: "サンプル",
-        lastNameKana: "タロウ",
-        email: "sample@test.com",
-    },
-    {
-        id: 5,
-        firstName: "5-サンプル",
-        lastName: "太郎",
-        firstNameKana: "サンプル",
-        lastNameKana: "タロウ",
-        email: "sample@test.com",
-    },
-];
+const { pages, staffs } = useStaffs();
 </script>
---
+++ src/types/staff.d.ts
@@ -0,0 +1,8 @@
+export type Staff = {
+    id: number;
+    firstName: string;
+    lastName: string;
+    firstNameKana: string;
+    lastNameKana: string;
+    email: string;
+};

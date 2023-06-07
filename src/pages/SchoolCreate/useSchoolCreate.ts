f0637acb25c4f8a306f473a4891f022db9e7189b
工藤隆弘 <>
2023-06-07 17:05:41 +0900

    chore : tagsの値はAPIでfetchした値を使うように修正

--- src/pages/SchoolCreate/useSchoolCreate.ts
+++ src/pages/SchoolCreate/useSchoolCreate.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useRouter } from "vue-router";
 import { useAlertStore } from "../../store/alert";
 import { School, SchoolParams } from "../../types/school";
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 
 export function useSchoolCreate() {
     const alertStore = useAlertStore();
@@ -26,38 +26,7 @@ export function useSchoolCreate() {
         link: "",
         recommendations: [],
         features: [],
-        tags: [
-            { id: 1, name: "オンライン通学可能1" },
-            { id: 2, name: "就職に強い1" },
-            { id: 3, name: "働きながら学習可能1" },
-            { id: 4, name: "オンライン通学可能2" },
-            { id: 5, name: "就職に強い2" },
-            { id: 6, name: "働きながら学習可能2" },
-            { id: 7, name: "オンライン通学可能3" },
-            { id: 8, name: "就職に強い3" },
-            { id: 9, name: "働きながら学習可能3" },
-            { id: 10, name: "オンライン通学可能4" },
-            { id: 11, name: "就職に強い4" },
-            { id: 12, name: "働きながら学習可能4" },
-            { id: 13, name: "オンライン通学可能5" },
-            { id: 14, name: "就職に強い5" },
-            { id: 15, name: "働きながら学習可能5" },
-            { id: 16, name: "オンライン通学可能6" },
-            { id: 17, name: "就職に強6" },
-            { id: 18, name: "働きながら学習可能6" },
-            { id: 19, name: "オンライン通学可能7" },
-            { id: 20, name: "就職に強い7" },
-            { id: 21, name: "働きながら学習可能7" },
-            { id: 22, name: "オンライン通学可能8" },
-            { id: 23, name: "就職に強い8" },
-            { id: 24, name: "働きながら学習可能8" },
-            { id: 25, name: "オンライン通学可能9" },
-            { id: 26, name: "就職に強い9" },
-            { id: 27, name: "働きながら学習可能9" },
-            { id: 28, name: "オンライン通学可能10" },
-            { id: 29, name: "就職に強い10" },
-            { id: 30, name: "働きながら学習可能10" },
-        ],
+        tags: [],
         selectedTagIds: [],
     });
 
@@ -71,5 +40,17 @@ export function useSchoolCreate() {
         }
     };
 
+    const fetchTags = async () => {
+        try {
+            const { data } = await axios.get("http://localhost:8080/api/tag");
+            school.value.tags = data;
+        } catch (err) {
+            alertStore.showErrorAlert();
+        }
+    };
+
+    onMounted(async () => {
+        fetchTags();
+    });
     return { pages, school, createSchool };
 }

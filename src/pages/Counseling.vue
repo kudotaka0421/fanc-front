625a8972d123d4dc762c1aa82babddcd0ffbfe4a
工藤隆弘 <>
2023-09-13 19:39:12 +0900

    feat: jiccoの埋め込み

--- src/pages/Counseling.vue
+++ src/pages/Counseling.vue
@@ -1,5 +1,8 @@
 <template>
-    <h1>カウンセリングページ</h1>
-    ※まだページとロジックが実装してないくfetchMEをしていないので、ユーザー情報取得できず、
-    ユーザー名表示や権限管理がうまくいっていない。useCounselingを作成した際にここは解決される予定。
+    <div>
+        <h1>カウンセリングページ</h1>
+        ※まだページとロジックが実装してないくfetchMEをしていないので、ユーザー情報取得できず、
+        ユーザー名表示や権限管理がうまくいっていない。useCounselingを作成した際にここは解決される予定。
+        <div class="jicoo-widget" data-url="https://www.jicoo.com/event_types/oippgJO-MPhM/widget" style="min-width:320px;height:720px;border:1px solid #e4e4e4;box-sizing:content-box;"></div>
+    </div>
 </template>

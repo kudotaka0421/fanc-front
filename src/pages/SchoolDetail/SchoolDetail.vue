7828ee4aa0c3b93996ba14b075447eb2a05ad2a7
工藤隆弘 <>
2023-09-21 16:50:10 +0900

    feat: スクール画面のリファクタ

--- src/pages/SchoolDetail/SchoolDetail.vue
+++ src/pages/SchoolDetail/SchoolDetail.vue
@@ -1,6 +1,6 @@
 <template>
     <Breadcrumbs :pages="pages" />
-    <School :school="school" :is-detail-page="true" @delete="deleteSchool" />
+    <School :school="school" @delete="deleteSchool" />
 </template>
 
 <script setup lang="ts">
--- src/pages/Schools/Schools.vue
+++ src/pages/Schools/Schools.vue
@@ -3,21 +3,22 @@
     <div class="sm:flex sm:items-center flex-row-reverse">
         <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none flex justify-end">
             <button
+                v-if="meStore.isAdmin"
                 type="button"
                 onclick="location.href='schoolCreate'"
                 class="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
-                作成する
+                新規作成
             </button>
         </div>
     </div>
-    <School v-for="school in schools" :school="school" :key="school.name" />
+    <SchoolList :schools="schools" />
 </template>
 
 <script setup lang="ts">
 import Breadcrumbs from "../../components/Breadcrumbs.vue";
-import School from "../../components/School/School.vue";
+import SchoolList from "../../components/School/SchoolList.vue";
 import { useSchools } from "./useSchools";
 
-const { pages, schools } = useSchools();
+const { pages, schools, meStore } = useSchools();
 </script>
